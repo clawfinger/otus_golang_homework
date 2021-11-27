3d253b116bbe16c8c4673b11960cb13dedec7f94
Dmitriy <clawfinger@yandex.ru>
2021-11-27 22:02:57 +0300

    asterisc task implementation

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,11 +9,13 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-type unpackState uint
+type previousSymbolState uint
 
 const (
-	EXPECTING_NON_DIGIT unpackState = 0
-	NORMAL              unpackState = 1
+	INITIAL previousSymbolState = 0
+	DIGIT   previousSymbolState = 1
+	LETTER  previousSymbolState = 2
+	ESCAPE  previousSymbolState = 3
 )
 
 func Unpack(source string) (string, error) {
@@ -22,32 +24,60 @@ func Unpack(source string) (string, error) {
 	}
 	var sb strings.Builder
 	var previous rune
-	var state unpackState = EXPECTING_NON_DIGIT
+	var state previousSymbolState = INITIAL
+	var needLast bool = true
 	for _, char := range source {
+		needLast = true
 		switch state {
-		case EXPECTING_NON_DIGIT:
+		case INITIAL:
 			if unicode.IsDigit(char) {
 				return "", ErrInvalidString
+			} else if char == '\\' {
+				state = ESCAPE
 			} else {
-				state = NORMAL
+				state = LETTER
 			}
-		case NORMAL:
+		case DIGIT:
 			if unicode.IsDigit(char) {
-				currentAsDigit, err := strconv.Atoi(string(char))
+				return "", ErrInvalidString
+			} else if char == '\\' {
+				sb.WriteRune(previous)
+				state = ESCAPE
+			} else {
+				state = LETTER
+			}
+		case LETTER:
+			if unicode.IsDigit(char) {
+				number, err := strconv.Atoi(string(char))
 				if err != nil {
 					return "", err
 				}
-				sb.WriteString(strings.Repeat(string(previous), currentAsDigit))
-				state = EXPECTING_NON_DIGIT
+				sb.WriteString(strings.Repeat(string(previous), number))
+				state = DIGIT
+				/* we need to add the last symbol to the result in all situations
+				except when the last operation on sequence was unpacking */
+				needLast = false
+			} else if char == '\\' {
+				sb.WriteRune(previous)
+				state = ESCAPE
 			} else {
 				sb.WriteRune(previous)
+				state = LETTER
+			}
+		case ESCAPE:
+			if unicode.IsDigit(char) {
+				state = LETTER
+			} else if char == '\\' {
+				state = LETTER
+			} else {
+				return "", ErrInvalidString
 			}
 		}
 		previous = char
+
 	}
-	if !unicode.IsDigit(previous) {
+	if needLast {
 		sb.WriteRune(previous)
 	}
-
 	return sb.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,18 +17,20 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// New tests
+		// tests
 		{input: " 5", expected: "     "},
 		{input: "a0b0c0", expected: ""},
 		{input: "qwe ert6", expected: "qwe ertttttt"},
 		{input: "куку", expected: "куку"},
 		{input: "куку3", expected: "кукууу"},
-		{input: "!@#$%*2()\\|2\\", expected: "!@#$%**()\\||\\"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		// New tests
+		{input: `qwe\\\\`, expected: `qwe\\`},
+		{input: `\1\2\3`, expected: `123`},
 	}
 
 	for _, tc := range tests {
