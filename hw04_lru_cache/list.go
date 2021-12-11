484dd1bdc25240f02db69a83b38a450db1069a6d
Dmitriy <clawfinger@yandex.ru>
2021-12-11 13:23:54 +0300

    list implemented

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,85 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	Head *ListItem
+	Tail *ListItem
+
+	len int
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.Head
+}
+
+func (l *list) Back() *ListItem {
+	return l.Tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+
+	if l.len == 0 {
+		l.Tail = item
+	} else {
+		head := l.Head
+		item.Next = head
+		head.Prev = item
+	}
+	l.Head = item
+
+	l.len++
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+	if l.len == 0 {
+		l.Head = item
+	} else {
+		tail := l.Tail
+		tail.Next = item
+		item.Prev = tail
+	}
+	l.Tail = item
+	l.len++
+	return item
+}
+
+func (l *list) removeImpl(i *ListItem) {
+	prev := i.Prev
+	next := i.Next
+	prev.Next = next
+}
+
+func (l *list) Remove(i *ListItem) {
+	current := l.Head
+	for current.Next != nil {
+		if i == current {
+			l.removeImpl(i)
+		}
+		current = current.Next
+	}
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == l.Head {
+		return
+	}
+	l.removeImpl(i)
+	l.PushFront(i.Value)
 }
 
 func NewList() List {
