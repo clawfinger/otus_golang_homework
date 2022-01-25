d800b5b85a18b4fa762099e977dfa8488f63c3b5
Dmitriy <clawfinger@yandex.ru>
2022-01-25 08:05:45 +0300

    fixed the solution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,56 +2,71 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func runner(task Task, errChan chan<- error, doneChan chan<- struct{}) {
-	err := task()
-	doneChan <- struct{}{}
-	if err != nil {
-		errChan <- err
+func runner(taskChan chan Task, errChan chan error, wg *sync.WaitGroup, stopped *int32) {
+End:
+	for {
+		select {
+		case task := <-taskChan:
+			err := task()
+			if atomic.LoadInt32(stopped) == 1 {
+				break End
+			}
+			errChan <- err
+		default:
+			if atomic.LoadInt32(stopped) == 1 {
+				break End
+			}
+		}
 	}
+	wg.Done()
 }
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	// Place your code here.
 
-	tasksLen := len(tasks)
+	taskCount := len(tasks) + 1
+	var wg sync.WaitGroup
+	tasksCounter := 0
 
 	errChan := make(chan error)
-	doneChan := make(chan struct{})
-	errCount := 0
+	taskChan := make(chan Task, taskCount)
 
-	pendingTasksIndex := -1
-	doneCounter := 0
-	if len(tasks) > n {
-		pendingTasksIndex = n
+	var stopped int32
+
+	errCount := 0
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go runner(taskChan, errChan, &wg, &stopped)
 	}
-	for i := 0; i < n || i < tasksLen; i++ {
-		go runner(tasks[i], errChan, doneChan)
+
+	for _, task := range tasks {
+		taskChan <- task
 	}
-Mark:
+	var res error
 	for {
-		select {
-		case <-doneChan:
-			doneCounter++
-			if doneCounter == tasksLen {
-				break Mark
-			}
-			if pendingTasksIndex > 0 && pendingTasksIndex < tasksLen {
-				go runner(tasks[pendingTasksIndex], errChan, doneChan)
-				pendingTasksIndex++
-			}
-		case <-errChan:
+		err := <-errChan
+		tasksCounter++
+		if err != nil {
 			errCount++
-			if errCount == m {
-				return ErrErrorsLimitExceeded
-			}
+		}
+		if errCount == m {
+			res = ErrErrorsLimitExceeded
+			break
+		}
+		if tasksCounter == len(tasks) {
+			break
 		}
 	}
-	return nil
+	atomic.StoreInt32(&stopped, 1)
+	wg.Wait()
+	return res
 }
