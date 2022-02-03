be82e09bf725a8158f0f8bf1cff42d274c746a02
Dmitriy <clawfinger@yandex.ru>
2022-02-03 18:53:32 +0300

    initial commit

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -9,7 +14,65 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const BufferLen = 5120
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+
+	count := 100
+	// create and start new bar
+	bar := pb.StartNew(count)
+
+	// finish bar
+	defer bar.Finish()
+	source, err := os.OpenFile(fromPath, os.O_RDONLY, os.ModeAppend)
+
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	dest, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	fi, err := source.Stat()
+
+	if err != nil {
+		return err
+	}
+
+	fileSize := fi.Size()
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = source.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+	var reader io.Reader
+	if limit == 0 {
+		reader = source
+	} else {
+		reader = io.LimitReader(source, limit)
+	}
+
+	totalWriten := 0
+
+	writeBuffer := bufio.NewWriter(dest)
+	readBuffer := make([]byte, BufferLen)
+	for {
+		read, err := reader.Read(readBuffer)
+
+		if err == io.EOF {
+			break
+		}
+		writeBuffer.Write(readBuffer[:read])
+		totalWriten += writeBuffer.Buffered()
+		writeBuffer.Flush()
+		bar.Set(totalWriten / int(fileSize) * 100)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/require"
+)
+
+func TestOffsetMoreThenLength(t *testing.T) {
+	from := "testdata/input.txt"
+	to := "testdata/out.txt"
+	err := Copy(from, to, 100000, 0)
+	require.Error(t, err)
+	require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	os.Remove(to)
+}
+
+func TestOffsetEqualLength(t *testing.T) {
+	from := "testdata/input.txt"
+	to := "testdata/out.txt"
+	err := Copy(from, to, 6617, 10)
+	defer os.Remove(to)
+
+	require.NoError(t, err)
+
+	result, err := os.OpenFile(to, os.O_RDONLY, os.ModeAppend)
+
+	require.NoError(t, err)
+
+	fi, err := result.Stat()
+
+	require.NoError(t, err)
+
+	fileSize := fi.Size()
+	require.Equal(t, int64(0), fileSize)
+	result.Close()
 }
