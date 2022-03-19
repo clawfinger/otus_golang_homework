2e132d7e5badfd8de10e02f18055fac7c8da3cad
Dmitriy <clawfinger@yandex.ru>
2022-03-19 17:36:13 +0300

    http handlers

--- hw12_13_14_15_calendar/internal/server/http/server.go
+++ hw12_13_14_15_calendar/internal/server/http/server.go
@@ -15,9 +15,15 @@ type Server struct { // TODO
 }
 
 func NewServer(serverCtx *servers.ServerContext) *Server {
-	handler := NewHandler(serverCtx.Logger)
+	handler := NewHandler(serverCtx)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", loggingMiddleware(handler.helloWorld, serverCtx.Logger))
+	mux.HandleFunc("/create", loggingMiddleware(handler.Create, serverCtx.Logger))
+	mux.HandleFunc("/update", loggingMiddleware(handler.Update, serverCtx.Logger))
+	mux.HandleFunc("/delete", loggingMiddleware(handler.Delete, serverCtx.Logger))
+	mux.HandleFunc("/getday", loggingMiddleware(handler.GetDay, serverCtx.Logger))
+	mux.HandleFunc("/getweek", loggingMiddleware(handler.GetWeek, serverCtx.Logger))
+	mux.HandleFunc("/getmonth", loggingMiddleware(handler.GetMonth, serverCtx.Logger))
+
 	server := &http.Server{
 		Addr:    serverCtx.Cfg.Data.HTTP.Addr,
 		Handler: mux,
